b03b5869f373cd0e2384fec3724b703598f88659
obicodeman555 <obicodeman@yahoo.com>
2020-09-03 12:16:28 -0700

    modified shop router link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 
 import ShopPage from "./pages/shop/shop.component";
 
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+import Header from "./components/header/header.component";
+
 function App() {
   return (
-    <di>
-      <Route exact path="/" component={HomePage} />
-
-      <Route path="/shop" component={ShopPage} />
-    </di>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
   );
 }
 
