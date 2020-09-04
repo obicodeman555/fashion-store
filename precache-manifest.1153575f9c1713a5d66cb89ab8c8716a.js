adb852d3eaeb079895a633de856dd41957c1d0e8
obicodeman555 <obicodeman@yahoo.com>
2020-09-03 19:47:16 -0700

    Updates

---
+++ precache-manifest.1153575f9c1713a5d66cb89ab8c8716a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5791d1f137dcaa1699399bbdd1993fc",
+    "url": "/fashion-store/index.html"
+  },
+  {
+    "revision": "daf67431cd46003083df",
+    "url": "/fashion-store/static/css/main.63450489.chunk.css"
+  },
+  {
+    "revision": "9a3246793d9a0fa661e1",
+    "url": "/fashion-store/static/js/2.b76f78d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fashion-store/static/js/2.b76f78d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf67431cd46003083df",
+    "url": "/fashion-store/static/js/main.cd1113fa.chunk.js"
+  },
+  {
+    "revision": "063f04025b00d4ae69f9",
+    "url": "/fashion-store/static/js/runtime-main.c5ae3c74.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/fashion-store/static/media/crown.ed28accb.svg"
+  }
+]);
